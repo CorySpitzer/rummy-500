3f91679785b0190ab110f377aa9ac48d6eecdc79
CorySpitzer <coryspitzer@gmail.com>
2014-04-07 15:40:20 -0400

    initial repository: rummy500.rb with class names

---
+++ rummy500.rb
@@ -0,0 +1,33 @@
+#  rummy500.rb
+#  A version of the rumy 500 card game 
+
+class Deck
+    def initialize()
+        #
+    end
+end
+    
+class Card
+    def initialize(suit, rank, score)
+        #@suit = suit
+        #@rank = rank
+    end
+end
+
+class Discard
+    def initialize()
+        #
+    end
+end
+
+class Hand
+    def initialize()
+        #
+    end
+end
+
+class Melds
+    def initialize(owner, top, bottom)
+        #
+    end
+end
